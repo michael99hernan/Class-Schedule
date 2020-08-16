b073eef47e2cb61ef3de217af92b1e1fd331ce14
michael99hernan <michael99hernandez@gmail.com>
2020-08-16 13:29:30 -0700

    Migration- Fixed nullable Fkey

---
+++ SchedulePicker/Data/Migrations/20200816202634_NullableMajorId.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SchedulePicker.Data.Migrations
+{
+    public partial class NullableMajorId : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Majors_MajorId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Majors_MajorId",
+                table: "AspNetUsers",
+                column: "MajorId",
+                principalTable: "Majors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Majors_MajorId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Majors_MajorId",
+                table: "AspNetUsers",
+                column: "MajorId",
+                principalTable: "Majors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
